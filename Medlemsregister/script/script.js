e00c0c12f82749bac6a7723edc9f6f3fca738463
lr222gw <lr222gw@student.lnu.se>
2014-01-09 19:40:37 +0100

    registrationsfältet är tillfälligt klart...

--- Medlemsregister/script/script.js
+++ Medlemsregister/script/script.js
@@ -9,8 +9,14 @@ var MEDLEMSREGISTER = {
 	},
 	
 	registerMember : function(){
-		var sendButton, name, lastName, cellphone, uniqueId, i, j, errorAlreadyExists;
-		errorAlreadyExists = 0;
+		var sendButton, name, lastName, cellphone, uniqueId, i, j, errorAlreadyExists, errorMessage, errorEnum;
+		errorAlreadyExists = 0;		
+		errorEnum ={
+			0 : "Namn",
+			1 : "Efternamn",
+			2 : "Telefonnummer"
+			
+		};
 		
 		sendButton = document.getElementById("sendbutton");
 		
@@ -28,26 +34,35 @@ var MEDLEMSREGISTER = {
 			//kontrollera att medlemmens namn inte redan finns
 			for(i = 0; i < MEDLEMSREGISTER.members.length; i +=1){ //kontrollera per medlem
 				
-				for(j = 0; j <= MEDLEMSREGISTER.members[i].length; j +=1){ // kontrollera en hel medlem.
+				errorAlreadyExists = 0; // nollställer denna för varje varv...
+				errorMessage ="Det finns redan en person med "; //nollställer denna för varje varv..
+				
+				for(j = 0; j < MEDLEMSREGISTER.members[i].length; j +=1){ // kontrollera en hel medlem.
 					
 					
-					if(MEDLEMSREGISTER.members[i][j] === regArr[j]){
-						alert("Whopse, namn/efternamn/telefonnummer är upptaget..");
+					if(MEDLEMSREGISTER.members[i][j] === regArr[j]){ // Denna sats kontrollerar om det redan finns en arrayplats i members som har samma egenskaper som de inladdade från sidan..
 						errorAlreadyExists +=1;
-						break;
 						
-					}									
+						errorMessage += "\n " + errorEnum[j]+ ": " + regArr[j]+ " " ;
+					}	
+					if(errorAlreadyExists === MEDLEMSREGISTER.members[i].length-1){// om längden på members nuvarande arrayobj är samma som 3
+						
+						alert(errorMessage);
+						break;
+					}							
 					
 				}
 			
-				while(MEDLEMSREGISTER.members[i][3] === uniqueId){ // om medlemens id ej är unikt, så ändra det till något som blir unikt..
-					uniqueId = i + name + (Math.random() *(1000 + 1)); 
-				}				
-
+				while(MEDLEMSREGISTER.members[i][3] === regArr[3]){ // om medlemens id ej är unikt, så ändra det till något som blir unikt..
+					regArr[3] = i + name + (Math.random() *(1000 + 1)); 
+				}
 			}
-			if(errorAlreadyExists === 0){// om inga namn är upptagna, lägg till den nya informationen.. efter givet unikt id..
+			
+			if(errorAlreadyExists <= 2){
 				MEDLEMSREGISTER.members.push(regArr);
 			}
+			
+			
 	
 		};
 		
