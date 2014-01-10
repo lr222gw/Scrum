a572f66a6c1c6f39bbafd63d3255c97060059920
lr222gw <lr222gw@student.lnu.se>
2014-01-10 17:15:30 +0100

    Prövar localstorage....

--- Medlemsregister/script/script.js
+++ Medlemsregister/script/script.js
@@ -104,25 +104,7 @@ var MEDLEMSREGISTER = {
 	},
 	
 	saveToDoc : function(regArr){ // Viss kod tagen härifrån: http://www.youtube.com/watch?v=woNQ2MA_0XU
-		var hr, url;
-		// Create our XMLHttpRequest object
-	    hr = new XMLHttpRequest();
-	    // Create some variables we need to send to our PHP file
-	    url = "save.php";
-
-	    hr.open("POST", url, true);
-	    // Set content type header information for sending url encoded variables in the request
-	    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	    // Access the onreadystatechange event for the XMLHttpRequest object
-	    hr.onreadystatechange = function() {
-		    if(hr.readyState === 4 && hr.status === 200) {
-			    var return_data = hr.responseText;
-				//document.getElementById("status").innerHTML = return_data;
-		    }
-	    };
-	    // Send the data to PHP now... and wait for response to update the status div
-	    hr.send(regArr); // Actually execute the request
-	    //document.getElementById("status").innerHTML = "processing...";
+	
 	},
 		
 	showMembers : function(){
